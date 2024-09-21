f827bdc34830d0d70316172005f7fd0faaaf2c91
Siroshun09 <siro.shun8@gmail.com>
2024-09-22 06:14:04 +0900

    feat: first version of serrors

---
+++ serrors.go
@@ -0,0 +1,147 @@
+package serrors
+
+import (
+	"errors"
+	"fmt"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
+type stackTraceError struct {
+	err        error
+	stackTrace StackTrace
+}
+
+func (e *stackTraceError) Error() string {
+	return e.err.Error()
+}
+
+func (e *stackTraceError) Unwrap() error {
+	return e.err
+}
+
+// New creates an error that has StackTrace.
+func New(msg string) error {
+	return withStackTrace(errors.New(msg))
+}
+
+// Errorf creates an error that has StackTrace.
+func Errorf(format string, args ...any) error {
+	return withStackTrace(fmt.Errorf(format, args...))
+}
+
+// WithStackTrace creates an error that has StackTrace.
+//
+// If err already has a StackTrace, this function returns err as-is.
+//
+// Also, if err is nil, this function returns nil.
+func WithStackTrace(err error) error {
+	return withStackTrace(err)
+}
+
+func withStackTrace(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	serr := getStackTraceError(err)
+	if serr != nil {
+		return serr
+	}
+
+	return &stackTraceError{
+		err:        err,
+		stackTrace: newStackTraceFromCallers(2), // withStackTrace -> caller (New/Errorf/WithStackTrace)
+	}
+}
+
+// GetStackTrace returns StackTrace from err.
+//
+// If err does not have StackTrace, this function creates current StackTrace.
+//
+// Also, if err is nil, this function returns nil.
+func GetStackTrace(err error) StackTrace {
+	if err == nil {
+		return nil
+	}
+
+	serr := getStackTraceError(err)
+	if serr != nil {
+		return serr.stackTrace
+	}
+
+	return newStackTraceFromCallers(1) // GetStackTrace
+}
+
+func getStackTraceError(err error) *stackTraceError {
+	if err == nil {
+		return nil
+	}
+
+	var serr *stackTraceError
+	if !errors.As(err, &serr) {
+		return nil
+	}
+
+	return serr
+}
+
+// FuncInfo has values that are obtained from runtime.Func.
+type FuncInfo struct {
+	// Name is a name of the function
+	Name string
+	// File is a filename of the function.
+	File string
+	// Line is a line number of the function.
+	Line int
+}
+
+// String formats FuncInfo as "name (file:line)"
+func (s FuncInfo) String() string {
+	return s.Name + " (" + s.File + ":" + strconv.Itoa(s.Line) + ")"
+}
+
+// StackTrace is a array of FuncInfo.
+type StackTrace []FuncInfo
+
+// String formats StackTrace using FuncInfo.String.
+func (s StackTrace) String() string {
+	builder := strings.Builder{}
+	for i, stack := range s {
+		if 0 < i {
+			builder.WriteString("\n")
+		}
+		builder.WriteString(stack.String())
+	}
+	return builder.String()
+}
+
+var unknownFuncInfo = FuncInfo{
+	Name: "UNKNOWN",
+}
+
+func newStackTraceFromCallers(skip int) StackTrace {
+	pcs := make([]uintptr, 64)
+	l := runtime.Callers(skip+2, pcs) // callers -> newStackTraceFromCallers
+
+	stacks := make(StackTrace, l)
+	for i, pc := range pcs[:l] {
+		stacks[i] = newFuncInfo(pc, runtime.FuncForPC(pc))
+	}
+
+	return stacks
+}
+
+func newFuncInfo(pc uintptr, f *runtime.Func) FuncInfo {
+	if f == nil {
+		return unknownFuncInfo
+	} else {
+		file, line := f.FileLine(pc)
+		return FuncInfo{
+			Name: f.Name(),
+			File: file,
+			Line: line,
+		}
+	}
+}
---
+++ serrors_test.go
@@ -0,0 +1,290 @@
+package serrors
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"runtime"
+	"testing"
+)
+
+func TestWithStackTrace(t *testing.T) {
+	tests := []struct {
+		name        string
+		err         error
+		wantNil     bool
+		wantSame    bool
+		wantWrapped bool
+	}{
+		{
+			name:    "nil",
+			err:     nil,
+			wantNil: true,
+		},
+		{
+			name:        "want wrapped err",
+			err:         errors.New("test"),
+			wantWrapped: true,
+		},
+		{
+			name:     "want same",
+			err:      New("test"),
+			wantSame: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := WithStackTrace(tt.err)
+			switch {
+			case tt.wantNil:
+				if got != nil {
+					t.Errorf("want nil, got %v", got)
+				}
+			case tt.wantSame:
+				{
+					if !reflect.DeepEqual(got, tt.err) {
+						t.Errorf("want %v, got %v", tt.err, got)
+					}
+				}
+			case tt.wantWrapped:
+				{
+					if !errors.Is(got, tt.err) {
+						t.Errorf("want %v, got %v", tt.err, got)
+					}
+				}
+			default:
+				t.Errorf("invalid test case")
+			}
+		})
+	}
+}
+
+func Test_getStackTraceError(t *testing.T) {
+	wrapped := New("wrapped")
+	tests := []struct {
+		name        string
+		err         error
+		wantSame    bool
+		wantWrapped bool
+		wantNil     bool
+	}{
+		{
+			name:     "get same (New)",
+			err:      New("test"),
+			wantSame: true,
+		},
+		{
+			name:     "get same (Errorf)",
+			err:      Errorf("%s", "test"),
+			wantSame: true,
+		},
+		{
+			name:     "get same (WithStackTrace)",
+			err:      WithStackTrace(errors.New("test")),
+			wantSame: true,
+		},
+		{
+			name:        "get wrapped (New)",
+			err:         fmt.Errorf("%w", wrapped),
+			wantWrapped: true,
+		},
+		{
+			name:    "get nil (errors.New)",
+			err:     errors.New("test"),
+			wantNil: true,
+		},
+		{
+			name:    "get nil (fmt.Errorf)",
+			err:     fmt.Errorf("%s", "test"),
+			wantNil: true,
+		},
+		{
+			name:    "get nil (nil)",
+			err:     nil,
+			wantNil: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := getStackTraceError(tt.err)
+
+			switch {
+			case tt.wantSame:
+				if !reflect.DeepEqual(got, tt.err) {
+					t.Errorf("getStackTraceError() = %v, want %v", got, tt.err)
+				}
+			case tt.wantWrapped:
+				if !reflect.DeepEqual(got, wrapped) {
+					t.Errorf("getStackTraceError() = %v, want %v", got, wrapped)
+				}
+			case tt.wantNil:
+				if got != nil {
+					t.Errorf("getStackTraceError() = %v, want %v", got, nil)
+				}
+
+			default:
+				t.Errorf("invalid test case")
+			}
+		})
+	}
+}
+
+func TestGetStackTrace(t *testing.T) {
+	stackTrace := StackTrace{}
+	tests := []struct {
+		name        string
+		err         error
+		want        StackTrace
+		wantCreated bool
+	}{
+		{
+			name: "expect nil",
+			err:  nil,
+			want: nil,
+		},
+		{
+			name: "expect stackTrace",
+			err:  &stackTraceError{err: errors.New("test"), stackTrace: stackTrace},
+			want: stackTrace,
+		},
+		{
+			name: "expect stackTrace (wrapped)",
+			err:  fmt.Errorf("%w", &stackTraceError{err: errors.New("test"), stackTrace: stackTrace}),
+			want: stackTrace,
+		},
+		{
+			name:        "expect stack trace created by GetStackTrace",
+			err:         errors.New("test"),
+			wantCreated: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.wantCreated {
+				if got := GetStackTrace(tt.err); got == nil {
+					t.Errorf("GetStackTrace() = %v, want created stack trace", got)
+				}
+			} else if got := GetStackTrace(tt.err); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetStackTrace() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFuncInfo_String(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		funcInfo FuncInfo
+		want     string
+	}{
+		{
+			name: "success",
+			funcInfo: FuncInfo{
+				Name: "Test",
+				File: "test.go",
+				Line: 10,
+			},
+			want: "Test (test.go:10)",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.funcInfo.String(); got != tt.want {
+				t.Errorf("String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStackTrace_String(t *testing.T) {
+	tests := []struct {
+		name       string
+		stackTrace StackTrace
+		want       string
+	}{
+		{
+			name:       "empty",
+			stackTrace: StackTrace{},
+			want:       "",
+		},
+		{
+			name: "1 FuncInfo",
+			stackTrace: StackTrace{
+				{
+					Name: "Test",
+					File: "test.go",
+					Line: 1,
+				},
+			},
+			want: "Test (test.go:1)",
+		},
+		{
+			name: "2 FuncInfo",
+			stackTrace: StackTrace{
+				{
+					Name: "Test1",
+					File: "test.go",
+					Line: 1,
+				},
+				{
+					Name: "Test2",
+					File: "test.go",
+					Line: 2,
+				},
+			},
+			want: "Test1 (test.go:1)\nTest2 (test.go:2)",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.stackTrace.String(); got != tt.want {
+				t.Errorf("String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_newFuncInfo(t *testing.T) {
+	pc, _, _, ok := runtime.Caller(0)
+	if !ok {
+		t.Fatalf("runtime error, can't get caller")
+	}
+	f := runtime.FuncForPC(pc)
+	if f == nil {
+		t.Fatalf("runtime error, can't get Func")
+	}
+	tests := []struct {
+		name        string
+		pc          uintptr
+		f           *runtime.Func
+		wantUnknown bool
+	}{
+		{
+			name:        "get unknown",
+			pc:          0,
+			f:           nil,
+			wantUnknown: true,
+		},
+		{
+			name:        "get actual",
+			pc:          pc,
+			f:           f,
+			wantUnknown: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := newFuncInfo(tt.pc, tt.f)
+			if tt.wantUnknown {
+				if got != unknownFuncInfo {
+					t.Errorf("newFuncInfo() = %v, want %v", got, unknownFuncInfo)
+				}
+			} else {
+				if got == unknownFuncInfo {
+					t.Errorf("newFuncInfo() = %v, want actual func info", unknownFuncInfo)
+				}
+			}
+		})
+	}
+}
